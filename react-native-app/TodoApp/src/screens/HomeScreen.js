3dc2f152cc00137226ef83a956dae3a66b22424e
How Cool Is Coding <howcooliscoding@gmail.com>
2023-04-08 00:52:30 +0900

    개선사항 반영

--- react-native-app/TodoApp/src/screens/HomeScreen.js
+++ react-native-app/TodoApp/src/screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { IconButton, TextInput, FAB, Text, List } from 'react-native-paper';
+import { IconButton, TextInput, FAB, Text, List, Portal, Dialog, Button } from 'react-native-paper';
 
 const HomeScreen = () => {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [editingTodo, setEditingTodo] = useState(null);
+  const [dialogVisible, setDialogVisible] = useState(false);
 
   const handleAddTodo = () => {
     if (inputValue.trim()) {
@@ -17,11 +19,38 @@ const HomeScreen = () => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const handleEditTodo = (id) => {
+    const todo = todos.find((todo) => todo.id === id);
+    if (todo) {
+      setInputValue(todo.text);
+      setEditingTodo(id);
+    }
+  };
+
+  const handleUpdateTodo = () => {
+    if (editingTodo && inputValue.trim()) {
+      setTodos(todos.map((todo) => todo.id === editingTodo ? { ...todo, text: inputValue } : todo));
+      setInputValue('');
+      setEditingTodo(null);
+    }
+  };
+
+  const handleCancelUpdate = () => {
+    setInputValue('');
+    setEditingTodo(null);
+  };
+
+  const showEditDialog = (id) => {
+    handleEditTodo(id);
+    setDialogVisible(true);
+  };
+
   const renderItem = (item) => {
     return (
       <List.Item
         key={item.id}
         title={item.text}
+        onPress={() => showEditDialog(item.id)}
         right={() => (
           <IconButton
             icon="delete"
@@ -35,13 +64,38 @@ const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <TextInput
-        label="Add a task"
+        label={editingTodo ? 'Edit task' : 'Add a task'}
         value={inputValue}
         onChangeText={setInputValue}
         style={styles.input}
       />
-      <FAB icon="plus" onPress={handleAddTodo} style={styles.fab} />
+      {editingTodo ? (
+        <View style={styles.editButtons}>
+          <Button onPress={handleUpdateTodo}>Save</Button>
+          <Button onPress={handleCancelUpdate}>Cancel</Button>
+        </View>
+      ) : (
+        <FAB icon="plus" onPress={handleAddTodo} style={styles.fab} />
+      )}
       {todos.map(renderItem)}
+
+      <Portal>
+        <Dialog visible={dialogVisible} onDismiss={() => setDialogVisible(false)}>
+          <Dialog.Title>Edit Task</Dialog.Title>
+          <Dialog.Content>
+            <TextInput
+              label="Edit task"
+              value={inputValue}
+              onChangeText={setInputValue}
+              style={styles.input}
+            />
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={() => { handleUpdateTodo(); setDialogVisible(false); }}>Save</Button>
+            <Button onPress={() => { handleCancelUpdate(); setDialogVisible(false); }}>Cancel</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
     </View>
   );
 };
@@ -59,6 +113,10 @@ const styles = StyleSheet.create({
     margin: 20,
     right: 0,
     bottom: 0,
+  },
+  editButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
 
