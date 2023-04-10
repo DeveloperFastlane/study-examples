d447d19f337fefe6a280f4e174421126426f6040
How Cool Is Coding <howcooliscoding@gmail.com>
2023-04-11 01:04:44 +0900

    improve home

--- instagram-clone/pages/explore.js
+++ instagram-clone/pages/explore.js
@@ -1,11 +1,146 @@
-// home.js, explore.js, new-post.js, activity.js, profile.js
+import React, { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
 import TabBar from '../components/TabBar';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-export default function Explore() {
+const Container = styled.div`
+  padding: 20px;
+`;
+
+const SearchBar = styled.input`
+  display: block;
+  width: 100%;
+  padding: 12px 20px;
+  margin: 0 0 20px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
+  grid-gap: 4px;
+`;
+
+const GridItem = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const SearchResultList = styled.ul`
+  position: absolute;
+  background-color: white;
+  width: 100%;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  max-height: 200px;
+  overflow-y: scroll;
+`;
+
+const SearchResultItem = styled.li`
+  padding: 8px 16px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f1f1f1;
+  }
+`;
+
+const Explore = () => {
+  const [images, setImages] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(0);
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  // Replace this function with a real API call
+  const fetchImages = async () => {
+    const totalPages = 3;
+    const pageSize = 9;
+    const allImages = [
+      ...Array(pageSize * totalPages)
+    ].map((_, index) => `https://via.placeholder.com/150?text=${index + 1}`);
+
+    if (page < totalPages) {
+      const newImages = allImages.slice(page * pageSize, (page + 1) * pageSize);
+      setImages((prevImages) => [...prevImages, ...newImages]);
+      setPage(page + 1);
+    } else {
+      setHasMore(false);
+    }
+  };
+
+  const handleSearch = (event) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+  
+    if (query.trim()) {
+      // Replace this with a real API call
+      const testUsers = [
+        { username: 'john_doe', name: 'John Doe' },
+        { username: 'jane_doe', name: 'Jane Doe' },
+        { username: 'bill_smith', name: 'Bill Smith' },
+      ];
+  
+      const testHashtags = ['#nature', '#photography', '#travel'];
+  
+      const filteredUsers = testUsers.filter((user) =>
+        user.username.toLowerCase().includes(query.toLowerCase())
+      );
+  
+      const filteredHashtags = testHashtags.filter((hashtag) =>
+        hashtag.toLowerCase().includes(query.toLowerCase())
+      );
+  
+      setSearchResults([...filteredUsers, ...filteredHashtags]);
+    } else {
+      setSearchResults([]);
+    }
+  };
+  
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
   return (
     <div>
-      <h1>Explore Page</h1> {/* Change this text for each page */}
+      <Container>
+        <SearchBar
+          type="text"
+          placeholder="Search for users or hashtags..."
+          value={searchQuery}
+          onChange={handleSearch}
+        />
+        {searchResults.length > 0 && (
+          <SearchResultList>
+            {searchResults.map((result, index) => (
+              <SearchResultItem key={index}>
+                {typeof result === 'string' ? result : result.username}
+              </SearchResultItem>
+            ))}
+          </SearchResultList>
+        )}
+        <InfiniteScroll
+          dataLength={images.length}
+          next={fetchImages}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+        >
+          <Grid>
+            {images.map((image, index) => (
+              <GridItem key={index} src={image} alt="Thumbnail" />
+            ))}
+          </Grid>
+        </InfiniteScroll>
+      </Container>
       <TabBar />
     </div>
   );
-}
+};
+
+export default Explore;
--- instagram-clone/pages/home.js
+++ instagram-clone/pages/home.js
@@ -1,11 +1,119 @@
-// home.js, explore.js, new-post.js, activity.js, profile.js
+import React, { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
 import TabBar from '../components/TabBar';
 
-export default function Home() {
+const Feed = styled.div`
+  margin: 0 auto;
+  max-width: 600px;
+  padding: 20px 0;
+`;
+
+const Post = styled.div`
+  background-color: white;
+  margin-bottom: 20px;
+  border-radius: 5px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+`;
+
+const PostHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 15px;
+  border-bottom: 1px solid #efefef;
+`;
+
+const User = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Avatar = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  margin-right: 10px;
+`;
+
+const Username = styled.span`
+  font-weight: 600;
+`;
+
+const PostImage = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+const PostActions = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+  border-bottom: 1px solid #efefef;
+`;
+
+const ActionButton = styled.button`
+  background: none;
+  border: none;
+  font-size: 20px;
+  margin-right: 15px;
+  cursor: pointer;
+`;
+
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  // Replace this function with a real API call
+  useEffect(() => {
+    // Fetch posts from API here
+    const fetchPosts = async () => {
+      const dummyPosts = [
+        {
+          id: 1,
+          user: {
+            username: 'user1',
+            avatar: 'https://via.placeholder.com/32',
+          },
+          imageUrl: 'https://via.placeholder.com/600x400',
+        },
+        {
+          id: 2,
+          user: {
+            username: 'user2',
+            avatar: 'https://via.placeholder.com/32',
+          },
+          imageUrl: 'https://via.placeholder.com/600x400',
+        },
+      ];
+
+      setPosts(dummyPosts);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div>
-      <h1>Home Page</h1> {/* Change this text for each page */}
+      <Feed>
+        {posts.map((post) => (
+          <Post key={post.id}>
+            <PostHeader>
+              <User>
+                <Avatar src={post.user.avatar} alt={post.user.username} />
+                <Username>{post.user.username}</Username>
+              </User>
+            </PostHeader>
+            <PostImage src={post.imageUrl} alt="Post" />
+            <PostActions>
+              <ActionButton>❤️</ActionButton>
+              <ActionButton>💬</ActionButton>
+              <ActionButton>🔗</ActionButton>
+            </PostActions>
+          </Post>
+        ))}
+      </Feed>
       <TabBar />
     </div>
   );
-}
+};
+
+export default Home;
