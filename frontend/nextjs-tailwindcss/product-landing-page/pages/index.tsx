805497f9577214efadf711b59d7129981df0bbdb
How Cool Is Coding <howcooliscoding@gmail.com>
2023-04-26 00:58:04 +0900

    add index page

--- frontend/nextjs-tailwindcss/product-landing-page/pages/index.tsx
+++ frontend/nextjs-tailwindcss/product-landing-page/pages/index.tsx
@@ -2,7 +2,7 @@ import HeroSection from '../components/HeroSection';
 import Features from '../components/Features';
 import CTASection from '../components/CTASection';
 import BlogSection from '../components/BlogSection';
-import NewsletterSection from '../components/NewsLetterSection';
+import NewsletterSection from '../components/NewsletterSection';
 
 export default function Home() {
   return (
